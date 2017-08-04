a8e74a1298e2c1f1b815dd588a31e6d99e442558
Jae Huh <jaeeunhuh@gmail.com>
2017-08-04 23:35:27 +1000

    Handle component layout - 8.54 App Wrapup

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,8 @@ import Header from './src/components/Header'
 import AlbumList from './src/components/AlbumList'
 
 const App = () => (
-  <View>
+  // Adding flex: 1 in the Root container of the app allows <ScrollView> in AlbumList.js to scroll all the way down instead of stopping before it reaches the bottom of the content.
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View>
